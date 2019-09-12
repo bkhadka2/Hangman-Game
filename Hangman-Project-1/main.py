5cd6de0b695e36a8bf127e381e29b56486fdf653
bkhadka2 <bishalkhadka1600@gmail.com>
2019-09-12 17:08:12 -0600

    Work in progress

--- Hangman-Project-1/main.py
+++ Hangman-Project-1/main.py
@@ -3,8 +3,8 @@ import random
 
 constant = 6
 
+# Hangman stand
 def hangmanStand():
-    # Hangman stand
     hangman.forward(130)
     hangman.setposition(65, 0)
     hangman.left(90)
@@ -14,94 +14,101 @@ def hangmanStand():
     hangman.right(90)
     hangman.forward(100)
 
-def hangmanHead():
-    # Hangman head
+
+def hangmanHead():  # Hangman head
     hangman.penup()
     hangman.setposition(145,230)
     hangman.pendown()
     hangman.circle(20)
 
-def hangmanBody():
+
+def hangmanBody(): # Hangman body
     hangman.penup()
     hangman.setposition(165, 210)
     hangman.pendown()
     hangman.fd(100)
 
-def hangmanLeftLeg():
-    # Hangman left leg
+
+def hangmanLeftLeg():  # Hangman left leg
     hangman.right(45)
     hangman.fd(30)
 
-def hangmanRightLeg():
+
+def hangmanRightLeg():  # Hangman right leg
     hangman.penup()
     hangman.setposition(165, 110)
     hangman.pendown()
     hangman.left(95)
     hangman.fd(30)
 
-def hangmanLeftHand():
-    # Hangman hands
+
+def hangmanLeftHand():  # Hangman left hand
     hangman.penup()
     hangman.setposition(165, 185)
     hangman.pendown()
     hangman.right(95)
     hangman.fd(30)
-def hangmanRightHand():
+
+
+def hangmanRightHand():  # Hangman right hand
     hangman.penup()
-    hangman.setposition(165,185)
+    hangman.setposition(165, 185)
     hangman.pendown()
     hangman.left(95)
     hangman.fd(30)
 
+
 def chooseField(fieldName):
     print("Welcome to {} section".format(fieldName))
-    musicDict = {}
+    musicDict = {}  # dictionary to keep key value pair of questions and answers
     file1 = open('music_Questions.txt', 'r')
     lines = file1.read().split(':')
+    print(len(lines))
     for i in range(0, len(lines) - 1, 2):
-        musicDict[lines[i]] = lines[i+1]
-    # print(musicDict)
-    # print(musicDict.values())
+        musicDict[lines[i]] = lines[i+1]  # assigning right key with right value after splitting the text
     question = random.choice(list(musicDict.keys()))
-    print(question)
-    astrisk = '*' * len(musicDict[question])
-    print(astrisk)
-    chanceCounter = 0
-    numberOfTimesPlayed = 0
-    response = False
+    print(question)  # displaying random questions to the player
+    asterisk = '*' * len(musicDict[question])
+    print(asterisk)
+    chanceCounter = 0  # Keeps track of number of chances player has
+    numberOfTimesPlayed = 0  # Keeps track of number of times player has played
+    response = False  # helper variable for new turtle window
     start = True
+
     while start:
-        if response:
+        if response: # Executes only when player wants to play the game again
             chanceCounter = 0
+            print('=' * 20)
             print("Welcome again")
-            hangman = turtle.Turtle()
+            print('=' * 20)
             turtle.Screen().reset()
-            turtle.bgcolor("green")
+            turtle.title("Welcome to Bishal's Hangman game")
             hangman.pensize(3)
             hangman.hideturtle()
-            turtle.title("Welcome to Bishalii's Hangman game")
             hangmanStand()
             question = random.choice(list(musicDict.keys()))
             print(question)
-            astrisk = '*' * len(musicDict[question])
-            print(astrisk)
+            asterisk = '*' * len(musicDict[question])
+            print(asterisk)
 
-        name = input("Enter your guessing character: ")
+        character = input("Enter your guessing character: ")
 
-        if name in musicDict[question]:
-            indices = []
+        if character in musicDict[question]:
+            indices = []  # finding the index of character/ repeated character and storing into it.
             findingChar = -1
             while True:
-                findingChar = musicDict[question].find(name, findingChar + 1)
+                findingChar = musicDict[question].find(character, findingChar + 1)  # to find the index of char
                 if findingChar == -1:
                     break
                 else:
                     indices.append(findingChar)
-            astrisk = list(astrisk)
+            asterisk = list(asterisk)
             for index in indices:
-                astrisk[index] = name
-            astrisk = "".join(astrisk)
-            print(astrisk)
+                asterisk[index] = character  # replace the '*' character with the appropriate letter if found
+            print('=' * 20)
+            asterisk = "".join(asterisk)  # joining the list with no spaces to make it look like a word.
+            print(asterisk)
+            print('=' * 20)
             response = False
 
         else:
@@ -110,7 +117,7 @@ def chooseField(fieldName):
             print("-> you have now {} chance/s left <-".format(constant - chanceCounter))
             if chanceCounter == 1:
                 hangmanHead()
-                response = False
+                response = False  # to prevent entering into the block of line 77 again and again.
             elif chanceCounter == 2:
                 hangmanBody()
                 response = False
@@ -125,21 +132,22 @@ def chooseField(fieldName):
                 response = False
             else:
                 hangmanRightHand()
-                turtle.write("Game Over")
+                turtle.write("Game Over", font=("Arial", 14, "normal"))
                 userInput = input("Do you want to play again(y/n)?: ")
                 if userInput == 'y':
                     numberOfTimesPlayed += 1
                     response = True
                 else:
                     numberOfTimesPlayed += 1
-                    print("You played {} time/s")
+                    print('=' * 35)
+                    print("You played {} time/s".format(numberOfTimesPlayed))
                     print("Good bye till next time >>> ")
+                    print('=' * 35)
+                    turtle.Screen().bye()
                     break
 
 
-
 if __name__ == '__main__':
-
     hangman = turtle.Turtle()
     wn = turtle.Screen()
     wn.bgcolor("green")
@@ -149,7 +157,6 @@ if __name__ == '__main__':
     print('=' * 35)
     print("Welcome to Bishal's hangman game")
     print('=' * 35)
-
     print("""
     Game Rules:
     1) You should guess the name within six tries.
@@ -159,4 +166,3 @@ if __name__ == '__main__':
     print('=' * 100)
     hangmanStand()
     chooseField("Music")
-    turtle.done()
